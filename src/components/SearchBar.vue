989d1d9ea11e9d0d957eeefd1578e8067cd30659
Tito-Blip <122133993+Tito-blip@users.noreply.github.com>
2024-09-05 09:27:53 -0400

    Add search bar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <ion-searchbar color="light" id="custom" placeholder="Custom"></ion-searchbar>
+  </template>
+  
+<script lang="ts" setup>
+    import { IonSearchbar } from '@ionic/vue';
+</script>
+
+<style>
+@media screen and (max-width: 600px) {
+    #custom {
+        margin: auto;
+    }
+}
+</style>
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,17 +2,18 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Home</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+      <ExploreContainer name="Home" />
+      <SearchBar />
     </ion-content>
   </ion-page>
 </template>
@@ -20,4 +21,5 @@
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
+import SearchBar from '@/components/SearchBar.vue';
 </script>
