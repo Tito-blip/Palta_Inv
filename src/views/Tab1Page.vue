fb3931399e5d81dc3879252127a91ae1c7e8fac1
Tito-Blip <122133993+Tito-blip@users.noreply.github.com>
2024-09-05 10:40:04 -0400

    ''

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,10 +1,15 @@
 <template>
   <ion-page>
+    
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="start">
+          <ion-menu-button :auto-hide="false"></ion-menu-button>
+        </ion-buttons>
         <ion-title>Home</ion-title>
       </ion-toolbar>
     </ion-header>
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
@@ -19,7 +24,7 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import {IonButtons, IonMenuButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
 import ExploreContainer from '@/components/ExploreContainer.vue';
 import SearchBar from '@/components/SearchBar.vue';
 </script>
