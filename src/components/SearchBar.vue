eb9f7da675fec7d4866731b47af5533293cd2408
Tito-Blip <122133993+Tito-blip@users.noreply.github.com>
2024-09-12 09:46:58 -0300

    Routes and Components

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,11 +5,3 @@
 <script lang="ts" setup>
     import { IonSearchbar } from '@ionic/vue';
 </script>
-
-<style>
-@media screen and (max-width: 600px) {
-    #custom {
-        margin: auto;
-    }
-}
-</style>
