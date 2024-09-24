982a57c927568b6a9602711d62e8a2f9d5aa9460
Tito-blip <122133993+Tito-blip@users.noreply.github.com>
2024-09-23 21:02:38 -0300

    Carousel

--- src/views/Home/HomePage.vue
+++ src/views/Home/HomePage.vue
@@ -6,19 +6,27 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Home</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Home" />
       <ion-searchbar color="secondary" id="custom" placeholder="Search"></ion-searchbar>
+      
+      <sl-carousel loop autoplay class="scroll-hint" pagination style="--scroll-hint: 10%;">
+        <sl-carousel-item>
+          <img src="https://picsum.photos/500?random=1">
+        </sl-carousel-item>
+        <sl-carousel-item>
+          <img src="https://picsum.photos/500?random=2">
+        </sl-carousel-item>
+        <sl-carousel-item>
+          <img src="https://picsum.photos/500?random=3">
+        </sl-carousel-item>
+        <sl-carousel-item>
+          <img src="https://picsum.photos/500?random=4">
+        </sl-carousel-item>
+      </sl-carousel>
+
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
