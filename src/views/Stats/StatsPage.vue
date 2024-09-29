86a2f058de661dc0f6f226a9dd30e72ac228ef0b
Tito-Blip <122133993+Tito-blip@users.noreply.github.com>
2024-09-29 00:21:41 -0300

    Stat placeholder

--- src/views/Stats/StatsPage.vue
+++ src/views/Stats/StatsPage.vue
@@ -1,29 +1,33 @@
 <template>
-    <ion-page>
-      <ion-header>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Stats</ion-title>
+        <ion-buttons slot="start">
+          <ion-menu-button></ion-menu-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <Menu />
+      <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title>Stats</ion-title>
-          <ion-buttons slot="start">
-            <ion-menu-button></ion-menu-button>
-          </ion-buttons>
+          <ion-title size="large">Stats</ion-title>
         </ion-toolbar>
       </ion-header>
-      <ion-content :fullscreen="true">
-        <Menu />
-        <ion-header collapse="condense">
-          <ion-toolbar>
-            <ion-title size="large">Stats</ion-title>
-          </ion-toolbar>
-        </ion-header>
+      <!-- | Placeholder | -->
+      <div class="ion-padding">
+        <v-sheet :elevation="6" :height="500" :width="500" border color="warning" rounded>
+          <Spark />
+        </v-sheet>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
   
-        <ExploreContainer name="Stats" />
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script setup lang="ts">
+<script setup lang="ts">
   import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/vue';
-  import ExploreContainer from '@/components/ExploreContainer.vue';
   import Menu from '@/components/Menu.vue';
-  </script>
+  import Spark from '@/components/Spark.vue';
+</script>
   
