cae86d534c5be51213875aac87ee3b224a7da83a
Tito-Blip <122133993+Tito-blip@users.noreply.github.com>
2024-10-31 18:27:45 -0300

    Forms

--- src/views/Forms/Add Product/AddProductForm.vue
+++ src/views/Forms/Add Product/AddProductForm.vue
@@ -1,11 +1,100 @@
 <template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar color="secondary">
+        <ion-title class="align-center">Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+  
+    <ion-content class="ion-padding d-flex justify-center align-center">
+      <form class="ma-auto" style="max-width: 400px; width: 100%;" @submit.prevent="submit">
+        <v-text-field
+          v-model="name.value.value"
+          :counter="10"
+          :error-messages="name.errorMessage.value"
+          label="Name"
+        ></v-text-field>
+  
+        <v-text-field
+          v-model="color.value.value"
+          :counter="7"
+          :error-messages="color.errorMessage.value"
+          label="Color"
+        ></v-text-field>
 
-</template>
+        <v-text-field
+            v-model="stock.value.value"
+            :error-messages="stock.errorMessage.value"
+            label="Stock"
+            type="number"
+        ></v-text-field>
+        
+        <!-- ! Change stock text field. Error: returns string -->
+        
+        <!--* TODO: Add missing fields. -->
 
-<style>
+        <v-btn
+          class="me-4"
+          type="submit"
+        >
+          submit
+        </v-btn>
+  
+        <v-btn @click="handleReset">
+          clear
+        </v-btn>
 
-</style>
+        <v-btn @click="$emit('close')"> Close </v-btn>
+      </form>
+    </ion-content>
+  </ion-page>
+  
+  </template>
+  
+  <script setup>
+    import { ref, defineEmits } from 'vue';
+    import { useField, useForm } from 'vee-validate';
+    import { IonPage, IonContent, IonHeader, IonToolbar, IonTitle } from '@ionic/vue';
 
-<script setup>
+    const emit = defineEmits(['close', 'addProductData']);
 
-</script>
+    const { handleSubmit, handleReset } = useForm({
+      validationSchema: {
+        name (value) {
+          if (value?.length >= 4) return true
+  
+          return 'Name needs to be at least 4 characters.'
+        },
+        color (value) {
+          if (value?.length >= 7) return true
+  
+          return 'Must be a valid e-mail.'
+        },
+        stock (value) {
+            if (value >= 0) return true
+
+            return 'Must be a valid number.'
+        },
+      } 
+    })
+  
+    const name = useField('name')
+    const color = useField('color')
+    const stock = useField('stock')
+
+    const submit = handleSubmit(() => {
+        const addProductData = {
+            name: name.value.value,
+            color: color.value.value,
+            stock: stock.value.value
+        }
+        emit('addProductData', addProductData);
+        emit('close');
+    })
+  </script>
+
+  <!--
+
+    submit => data => [{formData}] => cards.value.push({ ...addProductData }); => v-for show cards 
+
+  -->
--- src/views/Forms/Login/LoginForm.vue
+++ src/views/Forms/Login/LoginForm.vue
@@ -1,37 +1,88 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar color="secondary">
-        <ion-buttons slot="start">
-          <ion-back-button></ion-back-button>
-        </ion-buttons>
-        <ion-title>Login</ion-title>
-      </ion-toolbar>
-    </ion-header>
+  <ion-header>
+    <ion-toolbar color="secondary">
+      <ion-buttons slot="start">
+        <ion-back-button></ion-back-button>
+      </ion-buttons>
+      <ion-title>Login</ion-title>
+    </ion-toolbar>
+  </ion-header>
 
-    <ion-content class="ion-padding d-flex justify-center align-center">
-      <form class="ma-auto" style="max-width: 400px; width: 100%;">
-        <v-text-field label="Email" placeholder="johndoe@gmail.com" class="mb-4"></v-text-field>
-        <v-text-field label="Password" placeholder="*****" class="mb-4"></v-text-field>
-        <v-checkbox label="Remember Me" color="success"></v-checkbox>
-        <div class="d-flex justify-space-between mt-4">
-          <v-btn color="success">Submit</v-btn>
-          <v-btn color="red">Cancel</v-btn>
-        </div>
-      </form>
-    </ion-content>
-  </ion-page>
+  <ion-content class="ion-padding d-flex justify-center align-center">
+    <form class="ma-auto" style="max-width: 400px; width: 100%;" @submit.prevent="submit">
+      <v-text-field
+        v-model="name.value.value"
+        :counter="10"
+        :error-messages="name.errorMessage.value"
+        label="Name"
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email.value.value"
+        :error-messages="email.errorMessage.value"
+        label="E-mail"
+      ></v-text-field>
+
+      <v-checkbox
+        v-model="checkbox.value.value"
+        :error-messages="checkbox.errorMessage.value"
+        label="Option"
+        type="checkbox"
+        value="1"
+      ></v-checkbox>
+
+      <v-btn
+        class="me-4"
+        type="submit"
+      >
+        submit
+      </v-btn>
+
+      <v-btn @click="handleReset">
+        clear
+      </v-btn>
+    </form>
+  </ion-content>
+</ion-page>
+
 </template>
 
 <script setup>
-  import { IonPage, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, IonContent } from '@ionic/vue';
-</script>
+  import { ref } from 'vue';
+  import { useField, useForm } from 'vee-validate';
+  import { IonPage, IonContent, IonHeader, IonToolbar, IonBackButton, IonButtons, IonTitle } from '@ionic/vue';
+  import router from '@/router';
 
-<style scoped>
-  .ion-content {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-  }
-</style>
+  const { handleSubmit, handleReset } = useForm({
+    validationSchema: {
+      name (value) {
+        if (value?.length >= 4) return true
+
+        return 'Name needs to be at least 4 characters.'
+      },
+      email (value) {
+        if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+        return 'Must be a valid e-mail.'
+      },
+      checkbox (value) {
+        if (value === '1') 
+          return true
+
+        return 'Must be checked.'
+      },
+    },
+  })
+
+  const name = useField('name')
+  const email = useField('email')
+  const checkbox = useField('checkbox')
+
+  const submit = handleSubmit(values => {
+    alert(JSON.stringify(values, null, 2))
+    router.push("/")
+    console.log(name.value.value, email.value.value, checkbox.value.value)
+    loggedIn.value = true
+  })
+</script>
